95dd65f3da1e2a0a67ec16b35bb5e99f6085e20e
sumi-kobayashi <163929085+sumi-kobayashi@users.noreply.github.com>
2026-04-26 06:03:10 -0700

    Update service worker to use new cache version

--- sw.js
+++ sw.js
@@ -1,26 +1,26 @@
-const CACHE_NAME = 'rep-timer-v4';
+const CACHE_NAME = ‘rep-timer-v5’;
 const ASSETS = [
-  './',
-  './index.html',
-  'https://cdn.jsdelivr.net/npm/chart.js@4/dist/chart.umd.min.js'
+‘./’,
+‘./index.html’,
+‘https://cdn.jsdelivr.net/npm/chart.js@4/dist/chart.umd.min.js’
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener(‘install’, e => {
+e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
-  self.clients.claim();
+self.addEventListener(‘activate’, e => {
+e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).then(r => {
-      const rc = r.clone();
-      caches.open(CACHE_NAME).then(c => c.put(e.request, rc));
-      return r;
-    }).catch(() => caches.match(e.request))
-  );
+self.addEventListener(‘fetch’, e => {
+e.respondWith(
+fetch(e.request).then(r => {
+const rc = r.clone();
+caches.open(CACHE_NAME).then(c => c.put(e.request, rc));
+return r;
+}).catch(() => caches.match(e.request))
+);
 });
