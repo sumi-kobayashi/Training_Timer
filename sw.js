e24b2a91d89ddd4a42684832786792e352cef381
sumi-kobayashi <163929085+sumi-kobayashi@users.noreply.github.com>
2026-04-27 13:35:15 -0700

    Fix syntax errors in service worker event listeners

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
 // Self-destruct: clear all caches and unregister
-self.addEventListener(‘install’, function(e) {
-self.skipWaiting();
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
 });
 
-self.addEventListener(‘activate’, function(e) {
-e.waitUntil(
-caches.keys().then(function(keys) {
-return Promise.all(keys.map(function(k) { return caches.delete(k); }));
-}).then(function() {
-return self.registration.unregister();
-}).then(function() {
-return self.clients.matchAll();
-}).then(function(clients) {
-clients.forEach(function(c) { c.navigate(c.url); });
-})
-);
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+    }).then(function() {
+      return self.registration.unregister();
+    }).then(function() {
+      return self.clients.matchAll();
+    }).then(function(clients) {
+      clients.forEach(function(c) { c.navigate(c.url); });
+    })
+  );
 });
 
-// Don’t cache anything - pass through to network
-self.addEventListener(‘fetch’, function(e) {
-e.respondWith(fetch(e.request));
+// Don't cache anything - pass through to network
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request));
 });
